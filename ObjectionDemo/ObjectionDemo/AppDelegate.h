8fba55ceeb036bb938024fb79c2deab6087f1c6b
lijunjie <itlijunjie@gmail.com>
2015-05-26 23:11:39 +0800

    add 添加ObjectionDemo
    add 添加ObjectionDemo

---
+++ ObjectionDemo/ObjectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectionDemo
+//
+//  Created by lijunjie on 5/26/15.
+//  Copyright (c) 2015 lijunjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectionDemo/ObjectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectionDemo
+//
+//  Created by lijunjie on 5/26/15.
+//  Copyright (c) 2015 lijunjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
