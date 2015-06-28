5d69a9472018cc3ca947a6ec34a4d05043640a0b
lijunjie <itlijunjie@gmail.com>
2015-06-28 22:28:51 +0800

    add playground
    add playground

--- SwiftDemo/SwiftDemo/MyPlayground.playground/Contents.swift
+++ SwiftDemo/SwiftDemo/MyPlayground.playground/Contents.swift
@@ -5,3 +5,48 @@ import UIKit
 var str = "Hello, playground"
 
 
+var myVarible = 42
+
+myVarible = 50
+
+let myConstent = 42
+
+let implicitInteger = 70
+
+let implicitDouble = 70.0
+
+let explicitDouble : Double = 70
+
+let explicitFloat : Float = 10
+
+let label = "The width is "
+
+let width = 94
+
+let widthLabel = label + String(width)
+
+let apples = 3
+
+let oranges = 5
+
+let appleSummray = "I have \(apples)"
+
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+var shoppingList = ["catfish", "water", "tulips", "bule paint"]
+
+shoppingList[1] = "bottole of water"
+
+var occupations = [
+    "Malcolm" : "Captain",
+    "Kaylee" : "Mechanic"
+]
+
+occupations["Jayne"] = "Public Relations"
+
+let emptyArray = [String]()
+
+
+let emptyDictionary = Dictionary<String, Float>()
+
+
