136454169f2c09bc63d0da98f31ff813d01a2616
lijunjie <itlijunjie@gmail.com>
2015-06-29 19:54:59 +0800

    modify 添加枚举学习代码
    modify 添加枚举学习代码

--- SwiftDemo/SwiftDemo/MyPlayground.playground/Contents.swift
+++ SwiftDemo/SwiftDemo/MyPlayground.playground/Contents.swift
@@ -5,3 +5,24 @@ import UIKit
 var str = "Hello, playground"
 
 
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        default:
+            return String (self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.Ace
+
+let aceRawCalue = ace.rawValue
+
+
