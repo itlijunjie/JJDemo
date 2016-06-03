b6f5824c17145e7a8ff1da7265f4bde9c4b4fb49
itlijunjie <itlijunjie@gmail.com>
2016-06-03 18:13:30 +0800

    modify 添加XIBDemo
    modify 添加XIBDemo

---
+++ XIBDemo/XIBDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XIBDemo
+//
+//  Created by lijunjie on 16/6/3.
+//  Copyright © 2016年 lijunjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XIBDemo/XIBDemo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  XIBDemo
+//
+//  Created by lijunjie on 16/6/3.
+//  Copyright © 2016年 lijunjie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    
+}
+
+@end
